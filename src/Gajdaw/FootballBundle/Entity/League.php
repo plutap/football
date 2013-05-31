dac47a7a01fc2113f8b567130501752b188a4c19
pabloo99 <pawel-mazur2@o2.pl>
2013-05-31 09:24:06 +0200

    League entity

---
+++ src/Gajdaw/FootballBundle/Entity/League.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Gajdaw\FootballBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * League
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Gajdaw\FootballBundle\Entity\LeagueRepository")
+ */
+class League
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="country", type="string", length=255)
+     */
+    private $country;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return League
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     * @return League
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return string 
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+}
