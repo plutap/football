0e8a2eb082e0627e506c85f4a8cdde839be63c23
pabloo99 <pawel-mazur2@o2.pl>
2013-05-31 10:09:21 +0200

    Fixture League 2

--- src/Gajdaw/FootballBundle/DataFixtures/ORM/LoadLeagues.php
+++ src/Gajdaw/FootballBundle/DataFixtures/ORM/LoadLeagues.php
@@ -1,8 +1,30 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: abak
- * Date: 31.05.13
- * Time: 09:59
- * To change this template use File | Settings | File Templates.
- */
+
+namespace Gajdaw\FootballBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Gajdaw\FootballBundle\Entity\League;
+use Symfony\Component\Yaml\Yaml;
+
+class LoadConference implements FixtureInterface
+{
+    function load(ObjectManager $manager)
+    {
+        $filename =
+            __DIR__ .
+                DIRECTORY_SEPARATOR . '..' .
+                DIRECTORY_SEPARATOR . '..' .
+                DIRECTORY_SEPARATOR . 'Data/league.yml';
+
+        $yml = Yaml::parse(file_get_contents($filename));
+        foreach ($yml as $item) {
+            $league = new League();
+            $league->setName($item['name']);
+            $league->setCountry($item['country']);
+            $manager->persist($league);
+        }
+        $manager->flush();
+
+    }
+}
